5c497a47254d7be1c9bbd889333273492cde8075
Jimmywhf <whf2802367166@163.com>
2022-03-21 10:06:36 +0800

    第一次提交

---
+++ views/Main.vue
@@ -0,0 +1,43 @@
+<template>
+    <el-container style="height: 100%">
+        <el-aside width="auto">
+            <CommonAside></CommonAside>
+        </el-aside>
+        <el-container>
+            <el-header>
+                <CommonHeader></CommonHeader>
+            </el-header>
+            <CommonTag></CommonTag>
+            <el-main>
+                <router-view></router-view>
+            </el-main>
+        </el-container>
+    </el-container>
+</template>
+
+<script>
+import CommonAside from '../src/components/CommonAside.vue'
+import CommonHeader from '../src/components/CommonHeader.vue'
+import CommonTag from '../src/components/CommonTag.vue'
+
+export default {
+    name: 'Main',
+    components: {
+        CommonAside,
+        CommonHeader,
+        CommonTag
+    },
+    data () {
+        return {}
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.el-header {
+        background: #333;
+    }
+.el-main {
+    padding-top: 0;
+}
+</style>
---
+++ views/login/login.vue
@@ -0,0 +1,84 @@
+<template>
+    <el-form class="login-container" label-width="100px" ref="form" :model="form" status-icon :rules="rules">
+        <h3 class="login_title">系统登录</h3>
+        <el-form-item label="用户名" label-width="80px" prop="username" class="username">
+            <el-input type="input" v-model="form.username" placeholder="请输入账号" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码"  label-width="80px" prop="password">
+            <el-input type="password" v-model="form.password" placeholder="请输入密码" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item class="login_submit">
+            <el-button class="login_submit" type="primary" @click="login">登录</el-button>
+        </el-form-item>
+    </el-form>
+</template>
+
+<script>
+// import Mock from 'mockjs'
+import {getMenu} from '../../api/data'
+
+export default {
+    name: 'mall',
+    data () {
+        return {
+            form: {
+
+            },
+            rules: {
+                username: [
+                    {required: true, message:'请输入用户名', trigger: 'blur'},
+                    {
+                        min: 3,
+                        message: "用户名不能少于3位",
+                        trigger: 'blur'
+                    }
+                ],
+                password: [
+                    {required: true, message:'请输入密码', trigger: 'blur'},
+                    
+                ]
+            }
+        }
+    },
+    methods: {
+        login() {
+            getMenu(this.form).then(({data :res}) => {
+                console.log(res,'res')
+                if(res.code === 20000) {
+                    this.$store.commit('clearMenu')
+                    this.$store.commit('setMenu', res.data.menu)
+                    this.$store.commit('setToken', res.data.token)
+                    this.$store.commit('addMenu', this.$router)
+                    this.$router.push({name: 'home'})
+                } else {
+                    this.$message.warning(res.data.message)
+                }
+            })
+            // const token = Mock.Random.guid()
+            // this.$store.commit('setToken', token)
+            // this.$router.push({name: 'home'})
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.login-container {
+    border-radius: 15px;
+    background-clip: padding-box;
+    margin: 100px auto;
+    width: 350px;
+    padding: 35px 35px 15px 35px;
+    background-color: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 25px #cac6c6;
+}
+.login_title {
+    margin: 0px auto 40px auto;
+    text-align: center;
+    color: #505458;
+}
+.login_submit {
+    margin: 10px auto 0px auto;
+}
+</style>
